ef002efa29c488fab078272b8fda7d45c3eb8931
quang <nguyenthequang080203@gmail.com>
2025-03-03 21:09:20 +0700

    format

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic';
+'use client';
 
-('use client');
+export const dynamic = 'force-dynamic';
 
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
