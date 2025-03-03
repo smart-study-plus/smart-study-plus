48ac63adbd01fe47aaf69553abacd25c60483275
quang <nguyenthequang080203@gmail.com>
2025-03-03 20:58:21 +0700

    typescript process env error fix

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -4,8 +4,8 @@
 
 import { createBrowserClient } from '@supabase/ssr';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
